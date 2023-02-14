7e6c0ffa02089d11476569a50eaacb69be14c12e
adrianbgo <adrianbgo@hotmail.com>
2023-02-14 16:01:43 -0500

    added BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import ErrorPage from "./pages/ErrorPage";
 
@@ -19,6 +23,8 @@ const router = createBrowserRouter([
 ]);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} fallbackElement={<Home />}/>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <RouterProvider router={router} fallbackElement={<Home />} />
+    </BrowserRouter>
   </React.StrictMode>
 );
