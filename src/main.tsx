12d494ac98ad9fb2b254beebd46f171a0fd5adb0
adrianbgo <adrianbgo@hotmail.com>
2023-02-14 14:58:21 -0500

    Fix

--- src/main.tsx
+++ src/main.tsx
@@ -19,6 +19,6 @@ const router = createBrowserRouter([
 ]);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} fallbackElement={<Home />} />
+    <RouterProvider router={router} fallbackElement={<Home />}/>
   </React.StrictMode>
 );
