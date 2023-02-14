a6d4f97d2d343a2cb19abe745f6a13e0f88a8bb1
adrianbgo <adrianbgo@hotmail.com>
2023-02-14 17:06:33 -0500

    HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,7 @@ import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />}>
-          <Home />
-        </Route>
-      </Routes>
+      <Home />
     </div>
   );
 }
